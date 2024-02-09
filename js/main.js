9e0e07e89a218e27ead29ed00b9fe4127ed3fbff
Dmitry1512 <dima.kirshchin1512@gmail.com>
2024-02-10 01:34:40 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
+let inputOne = document.querySelector('.cube__input-one');
+let inputTwo = document.querySelector('.cube__input-two');
+const btn = document.querySelector('.cube__btn-result');
+const form = document.querySelector('.cube__form-control');
+let strongOne = document.querySelector('.strong-one');
+let strongTwo = document.querySelector('.strong-two');
+let strongThree = document.querySelector('.strong-three');
+const pitVolume = 3;
+let inHeight;
+let outHeight;
 
+function inCubes(inHeight) {
+  return inHeight * 3;
+}
+function outCubes(outHeight) {
+  return outHeight * 3;
+}
 
+function inOutSum(inCub, outCub) {
+  return inCub - outCub;
+}
+
+console.log(form);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  inHeight = inputOne.value;
+  outHeight = inputTwo.value;
+  let inCub = inCubes(inHeight);
+  let outCub = outCubes(outHeight);
+  let outSum = inOutSum(inCub, outCub);
+  strongOne.textContent = `${inCub.toFixed(2)} м³`;
+  strongTwo.textContent = `${outCub.toFixed(2)} м³`;
+  strongThree.textContent = `${outSum.toFixed(2)} м³`;
+});
